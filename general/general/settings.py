7bf8f7fd4401bd8cc4ced6d1ca3f4bd2fe1bf3d2
Aleht777 <lextinandrey@yandex.ru>
2021-09-12 17:34:30 +0300

    5_work

--- general/general/settings.py
+++ general/general/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -28,7 +29,7 @@ ALLOWED_HOSTS = [
     '127.0.0.1',
     'testserver',
     'www.Andrey1989.pythonanywhere.com',
-    'Andrey1989..pythonanywhere.com',
+    'Andrey1989.pythonanywhere.com',
 ]
 
 
@@ -123,6 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
