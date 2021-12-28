1358c15c8605a79c5975b00f1b4927dd5dea3538
Aleht777 <lextinandrey@yandex.ru>
2021-12-28 14:35:53 +0000

    mts_check_and_in

--- general/general_app/management/commands/mts_check_1.py
+++ general/general_app/management/commands/mts_check_1.py
@@ -6,7 +6,8 @@ from general_app.models import OperatorsSim, SimCards
 
 
 class Command(BaseCommand):
-    help = 'Проверка Симкарт Мегафон по колличеству'
+    help = 'Проверка Симкарт МТС по колличеству, между Проектои и сайтом' \
+           ', с выводом недостающих симкарт в cvs'
 
     def handle(self, *args, **options):
         path = 'general_app/management/commands/mts.csv'
@@ -39,13 +40,3 @@ class Command(BaseCommand):
                 if sim.icc not in icc_website:
                     print(f'{sim.number}, {sim.icc} - на проекте есть, на МТС нет')
         write.close()
-
-#        with open(path, 'r', newline='', encoding='latin-1') as data:
-#            result = csv.DictReader(data, delimiter=';')
-#            operator = OperatorsSim.objects.get(name='Мегафон')
-#            sum_sim_list = SimCards.objects.filter(operator=operator).count()
-#            all_site = 0
-#            for line in result:
-#                all_site += 1
-#            difference = all_site - sum_sim_list
-#            print(f'На сайте {all_site}, на проекте {sum_sim_list}, разница {difference} шт.')
---
+++ general/general_app/management/commands/mts_in_2.py
@@ -0,0 +1,32 @@
+import csv
+
+from django.core.management.base import BaseCommand
+
+from general_app.models import Human, HumanSimPresence, OperatorsSim, SimCards
+
+
+class Command(BaseCommand):
+    help = 'Занесение симкарт МТС в базу'
+
+    def handle(self, *args, **options):
+        path = 'general_app/management/commands/mts.csv'
+        with open(path, 'r', newline='', encoding='utf-8-sig') as data:
+            operator = OperatorsSim.objects.get(name='МТС')
+            human = Human.objects.get(last_name='Лехтин')
+            result = csv.DictReader(data, delimiter=';')
+            for line in result:
+                number = line['Абонентский номер'][1:]
+                icc = line['Номер SIM-карты'].strip()
+                if SimCards.objects.filter(number=number).exists():
+                    pass
+                else:
+                    SimCards.objects.create(
+                        operator=operator,
+                        number=number,
+                        icc=icc
+                    )
+                    sim = SimCards.objects.get(icc=icc)
+                    HumanSimPresence.objects.create(
+                        human=human,
+                        simcard=sim
+                    )
