16ca98ed66567a8d26b301e23632d2dfe4d2dbb2
Dru <lextand@gmail.com>
2024-10-15 15:02:06 +0300

    add date sort 2

--- general/general_app/views.py
+++ general/general_app/views.py
@@ -141,6 +141,7 @@ def server_view(request, server_id):
         JOIN general_app_wialonuser ON general_app_wialonuser.id = general_app_wialonobject.wialon_user_id
         JOIN general_app_humannames ON general_app_humannames.id = general_app_human.name_id_id
         WHERE general_app_wialonuser.server_id = %s
+        AND general_app_wialonobject.date_change_status > date('now')
         AND general_app_wialonobject.active = 1
         GROUP BY general_app_human.id
         ORDER BY general_app_humannames.name, general_app_human.last_name
